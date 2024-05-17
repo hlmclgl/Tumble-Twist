a400020a4e4960790656b3c8cbdce9c740e101c7
Ahmet Hilmi Çiloğlu <hlmclgl@gmail.com>
2024-05-18 01:28:48 +0300

    17.05.2024 last commit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,9 +7,12 @@ public class Ball : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private GameObject splashImg;
     [SerializeField] private float jumpForce;
+    private GameManager gm;
+    private Ring ring;
+
     void Start()
     {
-
+        gm = GameObject.FindObjectOfType<GameManager>();
     }
 
 
@@ -21,15 +24,15 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         rb.AddForce(Vector3.up * jumpForce);
-        GameObject splash = Instantiate(splashImg, transform.position, transform.rotation);
-        //splash.transform.SetParent(other.gameObject.transform);
-        //Destroy(splash, 1);
+        GameObject splash = Instantiate(splashImg, transform.position - new Vector3(0, 0.22f, 0f), transform.rotation);
+        splash.transform.SetParent(other.gameObject.transform);
+        Destroy(splash, 1);
 
         string metarialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
         Debug.Log(metarialName);
-        //if (metarialName == "Unsafe Color (Instance)")
-        //{
-        //    gm.restartGame();
-        //}
+        if (metarialName == "Unsafe Color (Instance)")
+        {
+            gm.restartGame();
+        }
     }
 }
