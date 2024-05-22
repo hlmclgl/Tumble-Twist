75898c24ef20200d58766fddea27e0dfe2c0b310
Ahmet Hilmi Çiloğlu <hlmclgl@gmail.com>
2024-05-22 03:39:08 +0300

    Pause menu developed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     public int score;
     public Text scoreText;
     public Text scoreNumberText;
--- Assets/Scripts/PauseButtonController.cs
+++ Assets/Scripts/PauseButtonController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseButtonController : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
     private bool isPaused = false;
 
     public void OnPauseButtonClick()
@@ -21,12 +24,30 @@ public class PauseButtonController : MonoBehaviour
     private void PauseGame()
     {
         Time.timeScale = 0f; // Oyunu duraklat
+        pauseMenu.SetActive(true);
         isPaused = true;
     }
 
     private void ResumeGame()
     {
         Time.timeScale = 1f; // Oyunu devam ettir
+        pauseMenu.SetActive(false);
         isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Zamaný tekrar baþlat
+        restartGame();
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu"); // Ana menü sahnesine geçiþ yapar
+    }
+
+    private void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
